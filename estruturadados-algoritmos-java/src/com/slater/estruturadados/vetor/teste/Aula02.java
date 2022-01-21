8cd1618d52920b9dbe3b798b4642b5c94107d0ce
Dheric Slater <dheric.slater@gmail.com>
2022-01-21 08:38:38 -0300

    adicionar-elemento

---
+++ estruturadados-algoritmos-java/src/com/slater/estruturadados/vetor/teste/Aula02.java
@@ -0,0 +1,15 @@
+package com.slater.estruturadados.vetor.teste;
+
+import com.slater.estruturadados.vetor.Vetor;
+
+public class Aula02 {
+
+	public static void main(String[] args) {
+		
+		Vetor vetor = new Vetor(5);
+		
+		int i = 0;
+		i++;
+	}
+
+}
